b7d0085b522580a07090ca83b95aea817022893c
= <ashillpathak@gmail.com>
2020-05-01 22:55:38 +0100

    created the service for player now i need to resolve the error as exception needs to be created for not finding player

---
+++ src/main/java/com/qa/service/playerService.java
@@ -0,0 +1,46 @@
+package com.qa.service;
+
+import com.qa.domain.Player;
+import com.qa.repo.playerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class playerService {
+    private final playerRepository repo;
+
+    @Autowired
+    public playerService(playerRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Player> readPlayers(){
+        return this.repo.findAll();
+    }
+
+    public Player createPlayer(Player player){
+        return this.repo.save(player);
+    }
+
+    public Player findPlayerById(Long id){
+        return this.repo.findById(id).orElseThrow(PlayerNotFoundException::new);
+    }
+
+    public Player updatePlayer(Long id, Player player){
+        Player update = findPlayerById(id);
+        update.setName(player.getName());
+        update.setGoals(player.getGoals());
+        update.setAssists(player.getAssists());
+        return this.repo.save(update);
+    }
+    public boolean deletePlayer(Long id){
+        if(!this.repo.existsById(id)){
+            throw new playerNotFoundException;
+        }
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
+    }
+
+}
