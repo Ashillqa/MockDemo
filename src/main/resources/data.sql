3b562af5e86b6198fe7ded7e5f48b7394f9d06b0
= <ashillpathak@gmail.com>
2020-05-13 18:54:27 +0100

    the sql file needs to be called data not just anything

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into category (id,level) values (1,'Beginner');
+insert into category (id,level) values (2,'Average');
+insert into category (id,level) values (3,'Elite');
--- src/main/resources/doc.sql
+++
@@ -1,3 +0,0 @@
-insert into category (id,level) values (1,"Beginner");
-insert into category (id,level) values (2,"Average");
-insert into category (id,level) values (3,"Elite");
