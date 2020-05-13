d89e757b566186fc7d9d489c4cf565bc9b3e055d
= <ashillpathak@gmail.com>
2020-05-13 16:41:50 +0100

    created js file to view all players which will also include buttons to delete and update
    in terms of update we only want them to change goals not their name - simplicity

---
+++ src/main/resources/static/JS/viewPlayers.js
@@ -0,0 +1,21 @@
+function getData() {
+    fetch('http://localhost:8080/getAllPlayers')
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            data.forEach((user) => {
+                const {id,name, goals} = user;
+                document.getElementById('result').innerHTML += `<div>
+                <ul>
+                    <li> Name : ${name}</li>
+                    <li> Goals : ${goals} </li>
+                    <button onclick="Delfunc(${id})">Delete</button><span>
+                    <p>Update goals</p><input id="U${id}" type="number" min="0">
+                    <button onclick="updP(${id},'${name}')">Update</button></span>
+                </ul>
+            </div>` ;
+            });
+
+        })
+}
